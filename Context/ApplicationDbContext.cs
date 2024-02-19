99d739adcb20fb449a10425111bbab903a7a7bbd
hk-kusumi <91217989+hk-kusumi@users.noreply.github.com>
2024-02-19 23:09:40 +0530

    first commit

---
+++ Context/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentCrudManagemant.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+
+        } 
+
+
+        public DbSet<Models.Student> Students { get; set; }
+    }
+
+
+}
---
+++ Models/Student.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentCrudManagemant.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = null!;
+        public string MiddleName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string FullName => $"{FirstName}  {MiddleName} {LastName}";
+
+        public string EmailAddress { get; set; } = null!;
+        public string Gender { get; set; } = null!;
+        public int PhoneNumber { get; set; }
+    }
+}
